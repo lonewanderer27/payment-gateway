f34615253a4ad2599bc81584ca9a8ef32a8cf088
lonewanderer27 <adrianejames27@gmail.com>
2023-08-16 01:55:22 +0800

    hide "redirecting back" if callbackUrl is missing

--- app/gcash/receipt/page.tsx
+++ app/gcash/receipt/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Divider, Spinner } from "@chakra-ui/react";
+import { Divider, Spinner } from "@chakra-ui/react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Receipt() {
@@ -9,18 +9,15 @@ export default function Receipt() {
   const merchant = searchParams.get("merchant");
   const amountDue = searchParams.get("amountDue");
   const referenceNo = searchParams.get("referenceNo");
+  const callbackUrl = searchParams.get("callbackUrl");
 
   return (
     <div className="h-screen flex flex-col items-center">
       <div>
-        <img
-          src="/gcash/logo.png"
-          alt="GCash Logo"
-          className="mt-20 h-14"
-        />
+        <img src="/gcash/logo.png" alt="GCash Logo" className="mt-20 h-14" />
       </div>
       <div className="rounded-md card bg-base-100 shadow-md px-5 sm:px-10 absolute top-52 py-8 max-[430px]:w-full sm:w-[400px]">
-        <div className="sm:py-4">
+        <span className="sm:py-4">
           <div className="flex justify-between py-1">
             <h2 className="card-title pb-2 text-gcash">{merchant}</h2>
             <ion-icon
@@ -40,22 +37,27 @@ export default function Receipt() {
             <span>Total</span>
             <span>₱ {amountDue}</span>
           </div>
-          <Divider />
-          <div className="flex justify-between py-3 mt-3">
-            <span className="text-sm">Redirecting you back to merchant</span>
-            <Spinner
-              size="md"
-              thickness="3px"
-              speed="1s"
-              className="text-gcash ml-3"
-            />
-          </div>
-          <button
-            className={`btn bg-gcash text-white btn-block btn-circle mb-3`}
-          >
-            Proceed
-          </button>
-          <div className="flex justify-between text-gcash-label">
+          {callbackUrl && (
+            <>
+              <div className="flex justify-between py-3 mt-3">
+                <span className="text-sm">
+                  Redirecting you back to merchant
+                </span>
+                <Spinner
+                  size="md"
+                  thickness="3px"
+                  speed="1s"
+                  className="text-gcash ml-3"
+                />
+              </div>
+              <button
+                className={`btn bg-gcash text-white btn-block btn-circle my-3`}
+              >
+                Proceed
+              </button>
+            </>
+          )}
+          <div className="flex justify-between text-gcash-label mt-5">
             <span>Date</span>
             <span>
               {new Date().toDateString()} {new Date().getHours()}:
@@ -67,7 +69,7 @@ export default function Receipt() {
             <span>Reference No.</span>
             <span>{referenceNo}</span>
           </div>
-        </div>
+        </span>
       </div>
     </div>
   );
